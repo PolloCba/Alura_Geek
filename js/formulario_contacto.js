95de3f44d3af89ff7bbe7d1e2e5e90dab8740980
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-04-27 17:14:07 -0300

    agreagada funcionalidad boton login y consola

--- js/formulario_contacto.js
+++ js/formulario_contacto.js
@@ -1,4 +1,4 @@
-const nombre = document.getElementById("nombre");
+/*const nombre = document.getElementById("nombre");
 const text = document.getElementById("mensaje");
 const form = document.getElementById("form");
 const parrafo = document.getElementById("warnings");
@@ -28,4 +28,36 @@ document.addEventListener('DOMContentLoaded', function(){
     formulario.addEventListener('submit', function() {
       formulario.reset();
     });
+  });*/
+
+  const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+  }
+
+  const formulario = document.getElementById("formulario");
+  const input = document.getElementById("nombre");
+  const textarea = document.getElementById("mensaje");
+  const parrafo = document.getElementById("warnings");
+
+  const validarFormulario = (evento)=>{
+      switch (evento.target.name){
+          case "nombre":
+             if (expresiones.nombre.test(evento.target.value)){
+                console.log("El nombre es correcto");
+             }else {
+                console.log("El nombre no es correcto");
+             }
+          break;
+      }
+  }
+
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+
+  formulario.addEventListener("submit", (evento)=>{
+      evento.preventDefault();
   });
